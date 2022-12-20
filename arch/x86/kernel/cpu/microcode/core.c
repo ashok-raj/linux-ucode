2d4e111535f8e56fe0ca315fc86adf4ce0fc912e
Ashok Raj <ashok.raj@intel.com>
2022-12-20 15:41:44 -0800

    x86/microcode: Move late load warning to the same function that taints kernel
    Currently the warning about late loading and tainting are issued from two
    different functions.
    
    Later patches will re-enable microcode late-loading.
    
    Having both messages in the same function helps issuing warnings only
    when required.
    
    Move the warning from microcode_reload_late() -> reload_store() where the
    kernel tainting also happens.
    
    No functional changes.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>
    Cc: LKML <linux-kernel@vger.kernel.org>
    Cc: x86 <x86@kernel.org>
    Cc: Tony Luck <tony.luck@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Alison Schofield <alison.schofield@intel.com>
    Cc: Reinette Chatre <reinette.chatre@intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Stefan Talpalaru <stefantalpalaru@yahoo.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- arch/x86/kernel/cpu/microcode/core.c
+++ arch/x86/kernel/cpu/microcode/core.c
@@ -441,9 +441,6 @@ static int microcode_reload_late(void)
 	int old = boot_cpu_data.microcode, ret;
 	struct cpuinfo_x86 info;
 
-	pr_err("Attempting late microcode loading - it is dangerous and taints the kernel.\n");
-	pr_err("You should switch to early loading, if possible.\n");
-
 	atomic_set(&late_cpus_in,  0);
 	atomic_set(&late_cpus_out, 0);
 
@@ -490,6 +487,9 @@ static ssize_t reload_store(struct device *dev,
 	tmp_ret = microcode_ops->request_microcode_fw(bsp, &microcode_pdev->dev);
 	if (tmp_ret != UCODE_NEW)
 		goto put;
+
+	pr_err("Attempting late microcode loading - it is dangerous and taints the kernel.\n");
+	pr_err("You should switch to early loading, if possible.\n");
 
 	mutex_lock(&microcode_mutex);
 	ret = microcode_reload_late();
