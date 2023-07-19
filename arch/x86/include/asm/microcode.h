ab50d931d202cd15f3f9759f663715cabbb631ab
Thomas Gleixner <tglx@linutronix.de>
2023-07-19 18:27:17 +0200

    Subject: x86/microcode: Rendevouz and load in NMI
    stop_machine() does not prevent the spin-waiting sibling from handling an
    NMI, which is obviously violating the whole concept of rendevouzing.
    
    Implement a static branch right in the beginning of the NMI handler which
    is NOOPed except when enabled by the late loading mechanism.
    
    The later loader enables the static branch before stop_machine() is
    invoked. Each CPU has an nmi_enable in its control structure which
    indicates whether the CPU should go into the udpate routine.
    
    This is required to bridge the gap between enabling the branch and actually
    being at the point where it makes sense.
    
    Each CPU which arrives in the stopper thread function sets that flag and
    issues a self NMI right after that. If the nmi function sees the flag
    clear, it returns. If it's set it clears the flag and enters the rendevouz.
    
    This is safe against a real NMI which hits in between setting the flag and
    sending the NMI to itself. The real NMI will be swallowed by the microcode
    update and the self NMI will then let stuff continue. Otherwise this would
    end up with a spurious NMI.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- arch/x86/include/asm/microcode.h
+++ arch/x86/include/asm/microcode.h
@@ -23,4 +23,16 @@ static inline void load_ucode_ap(void)				{ }
 static inline void microcode_bsp_resume(void)			{ }
 #endif
 
+bool microcode_nmi_handler(void);
+
+#ifdef CONFIG_MICROCODE_LATE_LOADING
+DECLARE_STATIC_KEY_FALSE(microcode_nmi_handler_enable);
+static __always_inline bool microcode_nmi_handler_enabled(void)
+{
+	return static_branch_unlikely(&microcode_nmi_handler_enable);
+}
+#else
+static __always_inline bool microcode_nmi_handler_enabled(void) { return false; }
+#endif
+
 #endif /* _ASM_X86_MICROCODE_H */
