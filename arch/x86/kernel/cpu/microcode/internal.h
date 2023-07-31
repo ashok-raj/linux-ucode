d23c9a9a90ddc41d66ff7d9b428586eae852d792
Ashok Raj <ashok.raj@intel.com>
2023-07-31 15:36:34 -0700

    x86/microcode/intel: Rip out mixed stepping support for Intel CPUs
    Mixed steppings aren't supported on Intel CPUs. Only one patch is required
    for the entire system.
    
    ucode_get_patch() currently returns "the" patch that is valid system wide.
    In subsequent patches that saves the patch only after a successful
    late-load, the function would return different patches depending on
    late-load or CPU hotplug update.
    
    Remove the microcode cache and simplify the code. Move the definition and
    use inside AMD code for private use.
    
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>

--- arch/x86/kernel/cpu/microcode/internal.h
+++ arch/x86/kernel/cpu/microcode/internal.h
@@ -11,16 +11,6 @@
 #include "amd.h"
 #include "intel.h"
 
-struct ucode_patch {
-	struct list_head plist;
-	void *data;		/* Intel uses only this one */
-	unsigned int size;
-	u32 patch_id;
-	u16 equiv_cpu;
-};
-
-extern struct list_head microcode_cache;
-
 struct device;
 
 enum ucode_state {
